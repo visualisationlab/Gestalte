44d599b790401d0f5abd585b3eba0c1b48248230
Joey <nahkranoth3@hotmail.com>
2025-07-30 21:25:36 +0200

    Added Conveyor Belt and stick to grid dragging

---
+++ Assets/Examples/Factory101/Scripts/ConveyorBelt.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public float pushStrength = 5f;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<McGibble>())
+        {
+            Debug.Log("Pushing");
+            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.right * pushStrength);  // Or transform.up depending on your belt direction
+        }
+    }
+}
