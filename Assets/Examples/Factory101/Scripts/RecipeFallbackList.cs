cb822a45e112f59af08f9ea71e6f935857dccec9
Joey vd Kaaij <j.g.vanderkaaij@uva.nl>
2025-08-01 13:21:59 +0200

    Added random FallBack Descriptions

---
+++ Assets/Examples/Factory101/Scripts/RecipeFallbackList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Examples.Factory101.Scripts
+{
+    [CreateAssetMenu(fileName = "FallbackDescriptions", menuName = "McGibble/FallbackDescriptions")]
+    public class McGibbleDescriptionFallbackList:ScriptableObject
+    {
+        public List<McGibbleDescription> descriptions;
+    }
+}
--- Assets/Examples/Factory101/Scripts/UIProgramMachineInfoPanel.cs
+++ Assets/Examples/Factory101/Scripts/UIProgramMachineInfoPanel.cs
@@ -7,11 +7,21 @@ public class UIProgramMachineInfoPanel : MonoBehaviour
     [SerializeField] private TextMeshProUGUI machineDescription;
     [SerializeField] private TextMeshProUGUI machineStatus;
 
+    private ExposeMachine currentMachine;
+    
     public void SetMachineInfo(ExposeMachine machine)
     {
+        currentMachine = machine;
         machineName.text = machine.name;
         machineDescription.text = machine.description;
         machineStatus.text = machine.GetStatus();
+    }
+    
+    
+    //After changing something kick it to update text
+    public void KickMachineInfo()
+    {
+        SetMachineInfo(currentMachine);
     }
     
 }
