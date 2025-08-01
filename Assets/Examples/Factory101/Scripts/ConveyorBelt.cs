1de32cd3b84fc6cab21b1f4bde7a7c4b1224e5ec
Jari Andersen <j-andersen@uva.nl>
2025-08-01 22:31:36 +0200

    Rollback conveyorbelt

--- Assets/Examples/Factory101/Scripts/ConveyorBelt.cs
+++ Assets/Examples/Factory101/Scripts/ConveyorBelt.cs
@@ -4,14 +4,14 @@ public class ConveyorBelt : MonoBehaviour, IBuyable
 {
     public float pushStrength = 5f;
     public int basePrice;
-    public Vector2 pushDirection = Vector2.right; // Editable in Inspector
+    
 
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<McGibble>())
         {
             Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-            rb.AddForce(pushDirection.normalized * pushStrength);
+            rb.AddForce(transform.right * pushStrength);  // Or transform.up depending on your belt direction
         }
     }
 
