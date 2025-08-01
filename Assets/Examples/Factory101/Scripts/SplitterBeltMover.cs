3c06d334af5fa3bc65ed14a3b636b9e5eb76e042
Jari Andersen <j-andersen@uva.nl>
2025-08-01 22:36:27 +0200

    Fixed Splitter belt

--- Assets/Examples/Factory101/Scripts/SplitterBeltMover.cs
+++ Assets/Examples/Factory101/Scripts/SplitterBeltMover.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 public class SplitterBeltMvoer : MonoBehaviour
 {
     public float pushStrength = 5f;
-    public Vector2 pushDirection = Vector2.right; // Editable in Inspector
+    public bool up = true;
 
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<McGibble>())
         {
             Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-            rb.AddForce(pushDirection.normalized * pushStrength);
+            Vector2 direction = up ? (Vector2)transform.up : -(Vector2)transform.up;
+            rb.AddForce(direction * pushStrength);  // Or transform.up depending on your belt direction
         }
     }
 }
