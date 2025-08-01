8141c329a41a0327241d8b0be105591e665a06cf
Joey vd Kaaij <j.g.vanderkaaij@uva.nl>
2025-08-01 18:05:27 +0200

    Fix for bug in resolving IBuyable

--- Assets/Examples/Factory101/Scripts/BuyableMachineButton.cs
+++ Assets/Examples/Factory101/Scripts/BuyableMachineButton.cs
@@ -5,5 +5,5 @@ public class BuyableMachineButton : MonoBehaviour
     [SerializeField]
     private MonoBehaviour buyableBehaviour; // assign a component that implements IBuyable in inspector
 
-    public IBuyable BuyableReference => buyableBehaviour as IBuyable;
+    public IBuyable BuyableReference => buyableBehaviour.GetComponent<IBuyable>();
 }
