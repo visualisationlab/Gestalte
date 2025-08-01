6277504aab06709d0eade959aab309c0a85f35c8
Joey vd Kaaij <j.g.vanderkaaij@uva.nl>
2025-08-01 18:15:42 +0200

    Added Hover Price Amount to ToolMenu

--- Assets/Examples/Factory101/Scripts/BuyableMachineButton.cs
+++ Assets/Examples/Factory101/Scripts/BuyableMachineButton.cs
@@ -6,4 +6,15 @@ public class BuyableMachineButton : MonoBehaviour
     private MonoBehaviour buyableBehaviour; // assign a component that implements IBuyable in inspector
 
     public IBuyable BuyableReference => buyableBehaviour.GetComponent<IBuyable>();
+
+    public void ShowHoverInfo()
+    {
+        CursorController.Instance.ShowMouseInfo(BuyableReference.GetPrice().ToString(), Color.yellow);
+    }
+    
+    public void HoverHide()
+    {
+        CursorController.Instance.HideMouseInfo();
+    }
+    
 }
