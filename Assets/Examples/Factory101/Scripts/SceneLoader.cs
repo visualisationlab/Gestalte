0d76f2d3db521463fad66d44ae2779d79d1ca14d
Joey <nahkranoth3@hotmail.com>
2025-08-03 10:05:30 +0200

    Start Menu added

--- Assets/Examples/Factory101/Scripts/SceneLoader.cs
+++ Assets/Examples/Factory101/Scripts/SceneLoader.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,25 +8,35 @@ public class SceneLoader : MonoBehaviour
     [Tooltip("Name of scene to load (must be in Build Settings)")]
     [SerializeField] private string sceneName;
 
-    public void LoadScene()
+    public void CallLoadGame()
     {
-        if (!Application.CanStreamedLevelBeLoaded(sceneName))
-        {
-            Debug.LogError($"Scene '{sceneName}' can't be loaded. Is it in Build Settings and spelled correctly?");
-            return;
-        }
-
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadSceneDelayedAsync(sceneName, 1f));
     }
 
-    public void LoadSceneFromString(string sceneName)
+    private IEnumerator LoadSceneDelayedAsync(string sceneName, float delay)
     {
         if (!Application.CanStreamedLevelBeLoaded(sceneName))
         {
             Debug.LogError($"Scene '{sceneName}' can't be loaded. Is it in Build Settings and spelled correctly?");
-            return;
+            yield break;
         }
 
-        SceneManager.LoadScene(sceneName);
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
+        op.allowSceneActivation = false; // hold activation
+
+        // Optional: you can surface op.progress (goes 0 to 0.9) to UI here.
+
+        float timer = 0f;
+        while (timer < delay)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        // After the delay, allow the scene to activate.
+        op.allowSceneActivation = true;
+
+        // Wait until the operation actually finishes.
+        yield return op;
     }
 }
